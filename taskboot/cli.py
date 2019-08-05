03d9d2333223adcecad3fa2e08773fcd89505499
Bastien Abadie <bastien@nextcairn.com>
2019-08-05 15:51:00 +0200

    Add magic comments

--- taskboot/cli.py
+++ taskboot/cli.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import argparse
 from taskboot.build import build_image, build_compose, build_hook
 from taskboot.push import push_artifacts, heroku_release
