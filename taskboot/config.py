30fb1adfc8c6615376ac56fb58d5a2ae359ab7ad
Dustin J. Mitchell <dustin@v.igoro.us>
2019-09-04 05:41:35 -0400

    Bug 1578593 - use taskcluster-urls to generate UI link (#58)

--- taskboot/config.py
+++ taskboot/config.py
@@ -12,7 +12,6 @@ import yaml
 logger = logging.getLogger(__name__)
 
 TASKCLUSTER_DEFAULT_URL = "https://taskcluster.net"
-TASKCLUSTER_DASHBOARD_URL = "https://tools.taskcluster.net"
 
 
 class Configuration(object):
@@ -30,6 +29,11 @@ class Configuration(object):
         if key in self.config:
             return self.config[key]
         raise KeyError
+
+    def get_root_url(self):
+        if "TASKCLUSTER_ROOT_URL" in os.environ:
+            return os.environ["TASKCLUSTER_ROOT_URL"]
+        return TASKCLUSTER_DEFAULT_URL
 
     def get_taskcluster_options(self):
         """
