mozilla/task-boot

commit 247c0699f687a68240851eee9fc6a99eaeea2ddc

Retry three times when pushing images through Skopeo

taskboot/docker.py
@@ -359,6 +359,8 @@ class Skopeo(Tool):
             cmd = [
                 "--debug",
                 "copy",
+                "--retry-times",
+                3,
                 "--authfile",
                 self.auth_file,
                 "docker-archive:{}".format(path),

commit 56c66b572dc9c3affa9f0ba0637fb92a59dc27a6

Pass number of retries as string, as expected by subprocess
Fixes a regression from 247c0699f687a68240851eee9fc6a99eaeea2ddc

taskboot/docker.py
@@ -360,7 +360,7 @@ class Skopeo(Tool):
                 "--debug",
                 "copy",
                 "--retry-times",
-                3,
+                "3",
                 "--authfile",
                 self.auth_file,
                 "docker-archive:{}".format(path),